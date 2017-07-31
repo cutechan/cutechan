d3ec39b635e20b6fdf0a57a90ab89153aac1753d
Kagami Hiiragi <kagami@genshiken.org>
2017-07-31 21:06:23 +0300

    Fix post counter for incomplete thread

--- ts/page/thread.ts
+++ ts/page/thread.ts
@@ -1,6 +1,12 @@
+import { THREAD_SEL } from "../vars"
 import { ThreadData } from "../common"
 import { page, loadFromDB } from "../state"
 import { isBanned, extractPost, extractPageData } from "./common"
+
+function setPostCounter(n: number) {
+  const thread = document.querySelector(THREAD_SEL)
+  thread.style.counterReset = `p ${n}`
+}
 
 // Render the HTML of a thread page.
 export async function render() {
@@ -13,6 +19,11 @@ export async function render() {
   const { threads: data, backlinks } = extractPageData<ThreadData>()
   const { posts } = data
   data.posts = null
+
+  // Shift post numbers if opened incomplete thread (+ 1 for OP post).
+  if (page.lastN === 100 && data.postCtr > 101) {
+    setPostCounter(data.postCtr - 101)
+  }
 
   extractPost(data, data.id, data.board, backlinks)
   for (let post of posts) {
