1eeee9437f8f5831203c9fb63ec392c74cf54c4d
Kagami Hiiragi <kagami@genshiken.org>
2017-08-03 14:56:17 +0300

    refactor: lint lang

--- ts/lang/index.ts
+++ ts/lang/index.ts
@@ -2,50 +2,53 @@
  * Follows lang.go structures.
  */
 
+// tslint:disable-next-line:no-reference
 /// <reference path="./index.d.ts" />
 
-import langs from "cc-langs"
+import langs from "cc-langs";
 
-type LanguagePack = {
-  Forms: { [key: string]: string[2] }
-  UI: { [key: string]: string }
-  Common: CommonLanguagePack
+// tslint:disable-next-line:interface-name
+interface LanguagePack {
+  Forms: { [key: string]: string[2] };
+  UI: { [key: string]: string };
+  Common: CommonLanguagePack;
 }
 
 // TODO(Kagami): Remove lowercase aliases.
-type CommonLanguagePack = {
-  Posts: { [key: string]: string }
-  posts: { [key: string]: string }
-  Sizes: { [key: string]: string }
-  Plurals: { [key: string]: [string] }
-  plurals: { [key: string]: [string] }
+// tslint:disable-next-line:interface-name
+interface CommonLanguagePack {
+  Plurals: { [key: string]: [string] };
+  Posts: { [key: string]: string };
+  Sizes: { [key: string]: string };
+  UI: { [key: string]: string };
+  plurals: { [key: string]: [string] };
+  posts: { [key: string]: string };
   time: {
-    calendar: string[]
-    week: string[]
-  }
-  UI: { [key: string]: string }
-  ui: { [key: string]: string }
+    calendar: string[],
+    week: string[],
+  };
+  ui: { [key: string]: string };
 }
 
 // TODO(Kagami): Add support for per-user site language.
-const siteLang: string = (window as any).config.defaultLang
-const pack: any = langs[siteLang]
+const siteLang: string = (window as any).config.defaultLang;
+const pack: any = langs[siteLang];
 
 // TODO(Kagami): Use `ln` everywhere.
 export const lang: CommonLanguagePack = {
-  Posts: pack.common.posts,
-  posts: pack.common.posts,
-  Sizes: pack.common.sizes,
   Plurals: pack.common.plurals,
-  plurals: pack.common.plurals,
-  time: pack.common.time,
+  Posts: pack.common.posts,
+  Sizes: pack.common.sizes,
   UI: pack.common.ui,
+  plurals: pack.common.plurals,
+  posts: pack.common.posts,
+  time: pack.common.time,
   ui: pack.common.ui,
-}
-export const ln: LanguagePack = { Forms: pack.forms, UI: pack.ui, Common: lang }
-export default lang
+};
+export const ln: LanguagePack = { Forms: pack.forms, UI: pack.ui, Common: lang };
+export default lang;
 
 // Very simple implementation.
 export function printf(s: string, ...args: any[]): string {
-  return s.replace(/%s/, () => args.shift())
+  return s.replace(/%s/, () => args.shift());
 }
