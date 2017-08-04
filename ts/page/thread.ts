d05e862e2ba525c33841dbb3ebfc0b8a41020d53
Kagami Hiiragi <kagami@genshiken.org>
2017-08-04 19:21:49 +0300

    Shift post counter on server side

--- ts/page/thread.ts
+++ ts/page/thread.ts
@@ -1,29 +1,16 @@
-import { THREAD_SEL } from "../vars"
 import { ThreadData } from "../common"
 import { page, loadFromDB } from "../state"
 import { isBanned, extractPost, extractPageData } from "./common"
 
-function setPostCounter(n: number) {
-  const thread = document.querySelector(THREAD_SEL)
-  thread.style.counterReset = `p ${n}`
-}
-
 // Render the HTML of a thread page.
 export async function render() {
-  if (isBanned()) {
-    return
-  }
+  if (isBanned()) return
 
   await loadFromDB(page.thread)
 
   const { threads: data, backlinks } = extractPageData<ThreadData>()
   const { posts } = data
   data.posts = null
-
-  // Shift post numbers if opened incomplete thread (+ 1 for OP post).
-  if (page.lastN === 100 && data.postCtr > 101) {
-    setPostCounter(data.postCtr - 101)
-  }
 
   extractPost(data, data.id, data.board, backlinks)
   for (let post of posts) {
