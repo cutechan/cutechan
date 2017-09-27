ab873290dfdabfaebff4337b6e6bf1dfec35cbd1
Kagami Hiiragi <kagami@genshiken.org>
2017-09-27 18:56:36 +0300

    Don't delete images used in stickers

--- go/src/meguca/db/sql/upkeep/delete_unused_files.sql
+++ go/src/meguca/db/sql/upkeep/delete_unused_files.sql
@@ -2,4 +2,5 @@ DELETE FROM images i
 WHERE
   NOT EXISTS (SELECT 1 FROM post_files WHERE file_hash = i.sha1)
   AND NOT EXISTS (SELECT 1 FROM image_tokens WHERE sha1 = i.sha1)
+  AND NOT EXISTS (SELECT 1 FROM stickers WHERE sha1 = i.sha1)
 RETURNING sha1, fileType, thumbType
