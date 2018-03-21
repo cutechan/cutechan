efa72284034e0ffc14824456b90b7220fe8335c3
Kagami Hiiragi <kagami@genshiken.org>
2018-03-21 13:51:43 +0300

    Move vmsg typings to its package

--- ts/util/modules.d.ts
+++ ts/util/modules.d.ts
@@ -5,16 +5,3 @@ declare module "textarea-caret" {
     {top: number, left: number};
   export = getCaretCoordinates;
 }
-
-declare module "vmsg" {
-  interface RecordOptions {
-    wasmURL?: string;
-    shimURL?: string;
-    pitch?: number;
-  }
-  interface Exports {
-    record: (opts?: RecordOptions) => Promise<Blob>;
-  }
-  const exports: Exports;
-  export default exports;
-}
