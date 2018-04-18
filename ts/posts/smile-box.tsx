ecfc47c8ac4fee11d0c2a7281feb63afd8e3b6fa
nunuju <yujinah@yandex.ru>
2018-04-18 21:52:42 +0300

    A couple of new thingsmiles and fixes for the old ones

--- ts/posts/smile-box.tsx
+++ ts/posts/smile-box.tsx
@@ -10,8 +10,9 @@ import { reverse, rotateRecent, setter } from "../util";
 
 const smileList = Array.from(smiles).sort();
 const thingSmiles = new Set(`
-  chips coffee cola corvalol goose gun gun2 heart heart2 heart3 heart4
-  karandash knife nogoose ovsyanka popcorn prev ramyun rope soju pringles
+  heart heart2 heart3 heart4 cigarette soju wine coffee 
+  cola chips goose nogoose gun gun2 karandash knife 
+  ovsyanka ramyun
 `.trim().split(/\s+/));
 const memeSmiles = new Set(`
   beast cool frukt heechul hyunsuk jyp jyp2 kwangsoo lookup priunil sooman
