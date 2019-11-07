52f984eb9b7b95ab28b0a9175be3b0e86e50d5ad
Kagami Hiiragi <kagami@genshiken.org>
2019-11-07 22:05:05 +0300

    Fix fs backend serving

--- go/src/meguca/server/init.go
+++ go/src/meguca/server/init.go
@@ -90,7 +90,7 @@ func createRouter(conf Config) http.Handler {
 		serveFile(w, r, cleanJoin(conf.SiteDir, "static", getParam(r, "path")))
 	})
 	if file.Backend.IsServable() {
-		r.GET(file.DefaultUploadsRoot+"*path", file.Backend.Serve)
+		r.GET(file.DefaultUploadsRoot+"/*path", file.Backend.Serve)
 	}
 	// Not yet in /etc/mime.types
 	mime.AddExtensionType(".wasm", "application/wasm")
